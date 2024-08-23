f8b0e713be297869a8fd2084162ae17911f380f6
elmurodvokhidov <elmurodvkh@gmail.com>
2024-08-23 16:50:41 +0500

    jwt token muddati 7 kun qilib belgilandi, privacy xossasi nodoby bo'lganlar endilikda qidiruvda chiqmaydi

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (auth, res) {
-    const token = jwt.sign({ auth: auth._id }, process.env.JWT_SECRET_KEY);
-    res.cookie("token", token, { maxAge: 15 * 24 * 60 * 60 * 1000 });
+    const token = jwt.sign({ auth: auth._id }, process.env.JWT_SECRET_KEY, { expiresIn: "7d" });
+    res.cookie("token", token, { maxAge: 7 * 24 * 60 * 60 * 1000 });
     res.status(200).send(auth);
 }
